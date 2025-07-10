64a8c60397f21a6410c3703357bbc079445f158f
Viraj Rajput <venomrajput007@gmail.com>
2025-07-10 06:05:13 +0000

    remove that circle from logo

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -6,7 +6,6 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg width="48" height="48" viewBox="0 0 120 120" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <rect width="120" height="120" rx="20" fill="hsl(var(--primary))"/>
-      <circle cx="60" cy="60" r="45" stroke="white" strokeWidth="1.5" fill="none"/>
       
       <g transform="translate(0, -5)">
         <path d="M60 32C49.507 32 41 40.507 41 51C41 65.5 60 78 60 78C60 78 79 65.5 79 51C79 40.507 70.493 32 60 32Z" fill="hsl(var(--primary))" stroke="white" strokeWidth="2.5"/>
